6006ad38bedcc670ae5e7c2501fdcad73151482b
Andrea-0319 <andreasc1903@gmail.com>
2026-04-12 22:57:03 +0200

    fix: remove invalid streamlit option

--- TaxiStreamlit.bat
+++ TaxiStreamlit.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=C:\Users\andre\AppData\Local\Programs\Python\Python311;%PATH%
 cd /d "%~dp0"
-python -m streamlit run llm_tool\StreamlitRania\app.py --server.folderWatchball false
+python -m streamlit run llm_tool\StreamlitRania\app.py
 pause
